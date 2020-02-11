c2e27c3ab3d9ac98ff82751f5d00e03fd63072b6
Jake Ash and Wei Dai <jake@>
2020-02-11 11:19:39 -0800

    Start farmers market project

--- src/components/MyStyledComponent.jsx
+++ src/components/MyStyledComponent.jsx
@@ -1,14 +1,15 @@
+import React from 'react';
+
 function MyStyledComponent(props) {
-    return (
-     <div>
-        <p className="red-text">only this paragraph will get the style :)</p>
-        <style jsx>{`
-          .red-text {
-            color: red;
-          }
-      `}</style>
+  return (
+    <div>
+      <h1>This is a Farmer's Market</h1>
+      <h2>We specialize in the freshest produce</h2>
+      <h3>All the products we have are seasonal</h3>
+      <h4>Everything we carry is local and organic</h4>
+      <p>Come check us out!</p>
     </div>
-    );
-  }
-  
-  export default MyStyledComponent;
+  );
+}
+
+export default MyStyledComponent;
